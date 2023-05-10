8227245566967b2fc7986563effc92f8aa156fc2
Lucas Tepper <lucas.tepper.91@gmail.com>
2023-05-10 13:02:48 +0200

    Fix time_subsample for tstart=0

--- plot_utils/utils.py
+++ plot_utils/utils.py
@@ -2,35 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def subsample_logspace(data: np.ndarray, n: int, dt: float, base=2):
+def get_log_func(base: float):
+    """Get the logarithm function for a given base,
+    numpy only has log naturalis, log2 and log10.
+    """
+    if base == 2:
+        return np.log2
+    elif base == 10:
+        return np.log10
+    elif np.abs(base - np.e) < 1e-2:
+        if not np.abs(base - np.e) < 1e-14:
+            print("Warning: Base is not exactly e, but close, might give artifacts.")
+        return np.log
+    else:
+        raise ValueError(f"Base must be 2, 10 or 2.718, but is {base}")
+
+
+def subsample_logspace(data: np.ndarray, n: int, dt: float, base: float = 2.0):
     """Subsample data logarithmically in time.
     Arguments:
         data: The data to subsample.
         n: The number of points to subsample.
         dt: The time step of the data.
-        base: The base of the logarithm (2, 10 or 'e'). default: 2
+        base: The base of the logarithm (2, 10 or 2.718). default: 2
     Returns:
         time: The time points of the subsampled data.
         data: The subsampled data.
     """
     if n >= len(data):
         return np.arange(len(data)) * dt, data
-    if base == 2:
-        end = np.log2(len(data))
-    elif base == 10:
-        end = np.log10(len(data))
-    elif base == "e":
-        base = np.e
-        end = np.log(len(data))
-    else:
-        raise ValueError(f"Base must be 2, 10 or 'e', but is {base}")
+    end = get_log_func(base)(len(data))
     idxs = np.unique(np.logspace(0, end, n, base=base, dtype=int)) - 1
     time = idxs * dt
     return time, data[idxs]
 
 
-def get_time_logspace(tstart: int, tend: int, n: int, dt: float, base=2):
+def get_time_logspace(tstart: float, tend: float, n: int, dt: float, base: float = 2.0):
     """Get a logarithmically spaced time array.
+    WARNING, this does not give exactly the same time array that subsample_logspace
+    gives, for some rounding reasons, at least when tstart is 0.
     Arguments:
         tstart: The start time.
         tend: The end time.
@@ -39,15 +49,18 @@ def get_time_logspace(tstart: int, tend: int, n: int, dt: float, base=2):
     Returns:
         time: The time points of the subsampled data.
     """
-    if base == 2:
-        start, end = np.log2(tstart), np.log2(tend)
-    elif base == 10:
-        start, end = np.log10(tstart), np.log10(tend)
-    else:
-        raise ValueError(f"Base was {base}, but needs to be 2 or 10.")
+    start_was_zero = False
+    if tstart == 0.0:
+        start_was_zero = True
+        tstart = dt
+        n -= 1
+    start = get_log_func(base)(tstart)
+    end = get_log_func(base)(tend)
     time = np.logspace(start, end, n, base=base) / dt
     # convert to int and filter unique
     time = np.unique(time.astype(int))
+    if start_was_zero:
+        time = np.insert(time, 0, 0)
     return time * dt
 
 
